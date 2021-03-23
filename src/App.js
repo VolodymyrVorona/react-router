486b092632fdeec41527a426a37bd5b6cc25af92
VolodymyrVorona <volodimir.vorona@gmail.com>
2021-03-23 23:51:57 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,12 @@
 import React from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
-import HomeView from './views/HomeView';
+import { Route } from 'react-router-dom';
+import HomeVeiw from './views/HomeView';
 import AuthorsView from './views/AuthorsView';
-import BooksView from './views/BooksView';
-import NotFoundView from './views/NotFoundView';
-import BookDetailsView from './views/BookDetailsView';
 
 const App = () => (
   <>
-    <ul>
-      <li>
-        <NavLink
-          exact
-          to="/"
-          className="NavLink"
-          activeClassName="NavLink--active"
-        >
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/authors"
-          className="NavLink"
-          activeClassName="NavLink--active"
-        >
-          Authors
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/books"
-          className="NavLink"
-          activeClassName="NavLink--active"
-        >
-          Books
-        </NavLink>
-      </li>
-    </ul>
-
-    <Switch>
-      <Route exact path="/" component={HomeView} />
-      <Route path="/authors" component={AuthorsView} />
-      <Route exact path="/books" component={BooksView} />
-      <Route path="/books/:bookId" component={BookDetailsView} />
-      <Route component={NotFoundView} />
-    </Switch>
+    <Route path="/" component={HomeVeiw} />
+    <Route path="/authors" component={AuthorsView} />
   </>
 );
 
